25e7b128c9d7453150bc53efc68fd2433577c9f1
pwysocki <01178616@pw.edu.pl>
2023-11-07 13:47:59 +0100

    test

--- src/main/java/pap/Pap.java
+++ src/main/java/pap/Pap.java
@@ -1,7 +1,16 @@
+
 package pap;
+import java.util.Scanner;
 
 public class Pap {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int x, y;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input first value: ");
+        x = input.nextInt();
+        System.out.print("Input second value: ");
+        y = input.nextInt();
+        System.out.println("x * y = " + x*y);
     }
 }
