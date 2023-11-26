14076044777abd7964b4064cb23ca1d40e7f6e99
pwysocki <01178616@pw.edu.pl>
2023-11-26 15:42:29 +0100

    Smieszne okno testowe

---
+++ src/main/java/pap/App.java
@@ -0,0 +1,33 @@
+package pap;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.layout.StackPane;
+import javafx.scene.Scene;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Dzien dobry");
+        btn.setOnAction(new EventHandler<>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                System.out.println("Dzien dobry");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 600, 400);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Papuga");
+        primaryStage.show();
+    }
+
+    public static void main (String[] args) {
+        launch(args);
+    }
+}
