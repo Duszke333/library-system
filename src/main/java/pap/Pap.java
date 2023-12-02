3d5c1d073521cf1a81e612845daba68dbe13dbfa
SuperNinja623 <bruno.sienkiewicz@gmail.com>
2023-12-02 14:16:36 +0100

    configuration files

--- src/main/java/pap/Pap.java
+++ src/main/java/pap/Pap.java
@@ -3,6 +3,5 @@ package pap;
 public class Pap {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        javafx.application.Application.launch(App.class);
     }
 }
