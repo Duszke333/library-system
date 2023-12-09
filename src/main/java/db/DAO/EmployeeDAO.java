f8ed694da5b79db9e159dea6633efbda111861e4
jkosla <01178539@pw.edu.pl>
2023-12-09 19:42:47 +0100

    Dodanie EmployeeDAO

---
+++ src/main/java/db/DAO/EmployeeDAO.java
@@ -0,0 +1,51 @@
+package db.DAO;
+
+import db.Entities.Employee;
+import db.SessionFactoryMaker;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class EmployeeDAO implements DAO<Employee>{
+    SessionFactoryMaker sessionFactoryMaker = new SessionFactoryMaker();
+    SessionFactory factory = sessionFactoryMaker.getSessionFactory();
+
+    public void create(Employee employee) {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.save(employee);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public Employee read(int id) {
+        Employee employee = null;
+        try (Session session = factory.openSession()) {
+            employee = session.get(Employee.class, id);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return employee;
+    }
+
+    public void update(Employee employee) {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.update(employee);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void delete(Employee employee) {
+        try (Session session = factory.openSession()) {
+            session.beginTransaction();
+            session.delete(employee);
+            session.getTransaction().commit();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
