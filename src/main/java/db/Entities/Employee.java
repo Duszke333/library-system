5888c0ae1682dcdaf24a60db493ae55258182c9f
pwysocki <01178616@pw.edu.pl>
2023-12-10 18:14:49 +0100

    Employee account creation added

--- src/main/java/db/Entities/Employee.java
+++ src/main/java/db/Entities/Employee.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.sql.Date;
+import java.util.Objects;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -23,17 +24,11 @@ public class Employee implements java.io.Serializable {
     @Column(name = "password_salt", nullable = false, length = 256)
     private String passwordSalt;
     @Basic
-    @Column(name = "first_name", nullable = false, length = 128)
-    private String firstName;
+    @Column(name = "username", nullable = false, length = 128)
+    private String username;
     @Basic
-    @Column(name = "last_name", nullable = false, length = 128)
-    private String lastName;
-    @Basic
-    @Column(name = "email", nullable = false, length = 128)
-    private String email;
-    @Basic
-    @Column(name = "address_id", nullable = false)
-    private int addressId;
+    @Column(name = "user_id", nullable = false)
+    private int userID;
     @Basic
     @Column(name = "date_created", nullable = false)
     private Date dateCreated;
@@ -58,15 +53,13 @@ public class Employee implements java.io.Serializable {
         Employee employee = (Employee) o;
 
         if (employeeId != employee.employeeId) return false;
-        if (addressId != employee.addressId) return false;
         if (active != employee.active) return false;
-        if (passwordHash != null ? !passwordHash.equals(employee.passwordHash) : employee.passwordHash != null) return false;
-        if (passwordSalt != null ? !passwordSalt.equals(employee.passwordSalt) : employee.passwordSalt != null) return false;
-        if (firstName != null ? !firstName.equals(employee.firstName) : employee.firstName != null) return false;
-        if (lastName != null ? !lastName.equals(employee.lastName) : employee.lastName != null) return false;
-        if (email != null ? !email.equals(employee.email) : employee.email != null) return false;
-        if (dateCreated != null ? !dateCreated.equals(employee.dateCreated) : employee.dateCreated != null) return false;
-        if (role != null ? !role.equals(employee.role) : employee.role != null) return false;
+        if (!Objects.equals(passwordHash, employee.passwordHash)) return false;
+        if (!Objects.equals(passwordSalt, employee.passwordSalt)) return false;
+        if (!Objects.equals(username, employee.username)) return false;
+        if (userID != employee.userID) return false;
+        if (!Objects.equals(dateCreated, employee.dateCreated)) return false;
+        if (!Objects.equals(role, employee.role)) return false;
         if (branchId != employee.branchId) return false;
         return true;
     }
@@ -77,10 +70,8 @@ public class Employee implements java.io.Serializable {
 
         result = 31 * result + (passwordHash != null ? passwordHash.hashCode() : 0);
         result = 31 * result + (passwordSalt != null ? passwordSalt.hashCode() : 0);
-        result = 31 * result + (firstName != null ? firstName.hashCode() : 0);
-        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
-        result = 31 * result + (email != null ? email.hashCode() : 0);
-        result = 31 * result + addressId;
+        result = 31 * result + (username != null ? username.hashCode() : 0);
+        result = 31 * result + userID;
         result = 31 * result + (dateCreated != null ? dateCreated.hashCode() : 0);
         result = 31 * result + (active ? 1 : 0);
         result = 31 * result + (role != null ? role.hashCode() : 0);
