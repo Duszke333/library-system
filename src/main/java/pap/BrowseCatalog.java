9db31c96ea3f03a7d1afadbdba5f309608292907
pwysocki <01178616@pw.edu.pl>
2023-12-10 21:04:45 +0100

    Added basic data to init.sql

--- src/main/java/pap/BrowseCatalog.java
+++ src/main/java/pap/BrowseCatalog.java
@@ -2,28 +2,60 @@ package pap;
 
 import db.Entities.Book;
 import db.Repository.BookRepository;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class BrowseCatalog {
-    private List<Book> books;
+public class BrowseCatalog implements Initializable {
     @FXML
     private TableView<Book> bookCatalog;
+    @FXML
+    private TableColumn<Book, String> bookTitle;
+    @FXML
+    private TableColumn<Book, String> bookAuthor;
+    @FXML
+    private TableColumn<Book, String> bookGenre;
+    @FXML
+    private TableColumn<Book, String> bookLanguage;
+    @FXML
+    private TableColumn<Book, String> bookPageCount;
+    @FXML
+    private TableColumn<Book, String> bookPublicationYear;
+    @FXML
+    private TableColumn<Book, String> bookPublisher;
+    @FXML
+    private TableColumn<Book, String> bookDescription;
+    @FXML
+    private TableColumn<Book, String> bookISBN;
 
-    public void initialize() {
-        //TODO
-        // get all books from database
-        // cos w stylu book = BookRepository.getAll();
-        // set books to table
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObservableList<Book> books = FXCollections.observableArrayList(new BookRepository().getAll());
         bookCatalog.widthProperty().addListener(o -> {
             bookCatalog.getColumns().forEach(column -> {
                 column.setMinWidth(bookCatalog.getWidth() / bookCatalog.getColumns().size());
             });
         });
+        bookCatalog.setItems(books);
+
+        bookTitle.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getTitle()));
+        bookAuthor.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getAuthor()));
+        bookGenre.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getGenre()));
+        bookLanguage.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getLanguage()));
+        bookPageCount.setCellValueFactory(cellData -> new SimpleStringProperty(String.valueOf(cellData.getValue().getPageCount())));
+        bookPublicationYear.setCellValueFactory(cellData -> new SimpleStringProperty(String.valueOf(cellData.getValue().getPublicationYear())));
+        bookPublisher.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getPublisher()));
+        bookDescription.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDescription()));
+        bookISBN.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getIsbn()));
     }
 }
