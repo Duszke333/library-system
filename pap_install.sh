3a109c452603fc7fad647568c7b9623be0604c7c
permito <penciller94@gmail.com>
2023-12-13 13:45:10 +0100

    Install script + jar

--- pap_install.sh
+++ pap_install.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-docker run -p 5432:5432 --name postgres_pap -e POSTGRES_PASSWORD=root -d postgres
-docker cp scripts/init_database.sql postgres_pap:/docker-entrypoint-initdb.d/initialize.sql
-docker exec -u postgres postgres_pap psql postgres postgres -f docker-entrypoint-initdb.d/initialize.sql
-mvn package
-mvn exec:java
+docker compose up --build &
+java -jar pap-0.1.jar
