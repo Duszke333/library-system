a80f430eb78a040c0761c1ce445710e9906e4ff4
jkosla <01178539@pw.edu.pl>
2024-01-04 15:23:01 +0100

    Dodanie WishRepository

---
+++ src/main/java/pap/db/Repository/WishRepository.java
@@ -0,0 +1,43 @@
+package pap.db.Repository;
+
+import pap.db.DAO.EntityDAO.BookWishListDAO;
+import pap.db.Entities.BookWishList;
+import pap.db.Repository.Interface.IBookWishList;
+
+import java.util.List;
+
+public class WishRepository implements IBookWishList {
+    private BookWishListDAO bookWishListDAO = new BookWishListDAO();
+
+    @Override
+    public List<BookWishList> getWishListByUserId(int id) {
+        String sql = "SELECT * FROM pap.wish_list WHERE user_id = " + id;
+        return bookWishListDAO.query(sql);
+    }
+    @Override
+    public List<BookWishList> getAll() {
+        return bookWishListDAO.getAll();
+    }
+
+    @Override
+    public BookWishList getById(int id) {
+        return bookWishListDAO.read(id);
+    }
+
+    @Override
+    public void create(BookWishList entity) {
+        bookWishListDAO.create(entity);
+    }
+
+    @Override
+    public void update(BookWishList entity) {
+        bookWishListDAO.update(entity);
+    }
+
+    @Override
+    public void delete(BookWishList entity) {
+        bookWishListDAO.delete(entity);
+    }
+
+
+}
