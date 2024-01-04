7ca03278431eb2bb2b5ab0147115998fa8f445ae
jkosla <01178539@pw.edu.pl>
2024-01-04 15:19:37 +0100

    Stworzenie BookWishListDAO

---
+++ src/main/java/pap/db/DAO/EntityDAO/BookWishListDAO.java
@@ -0,0 +1,19 @@
+package pap.db.DAO.EntityDAO;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import pap.db.DAO.GenericDAO;
+import pap.db.Entities.BookWishList;
+
+import java.util.List;
+
+public class BookWishListDAO extends GenericDAO<BookWishList> {
+
+    public BookWishListDAO() {
+        super(BookWishList.class, "pap.wish_list");
+    }
+
+    public BookWishListDAO(SessionFactory factory) {
+        super(BookWishList.class, "pap.wish_list", factory);
+    }
+}
