5f5488d37d92034238bb7ed99a5ab3b2e67a5cee
permito <penciller94@gmail.com>
2024-01-08 18:19:06 +0100

    Script doesn't work :( (yet)

--- pap.py
+++ pap.py
@@ -5,24 +5,35 @@ import sys
 import warnings
 import os
 
-RUN_DB_CMD = ["docker", "compose", "up", "--build"]
+
+def run_db_cmd(package_manager: str):
+    if package_manager == "dnf":
+        return ["docker-compose", "up", "--build"]
+    else:
+        return ["docker", "compose", "up", "--build"]
 
 
 def main(argv):
-    opts, args = getopt.getopt(argv, "hdif", ["help", "database", "install", "fix"])
+    opts, _ = getopt.getopt(argv, "hdif", ["help", "database", "install", "fix"])
     if len(opts) == 0:
         opts = [("--help", None)]
 
-    for opt, arg in opts:
+    if os.path.exists("/usr/bin/apt"):
+        package_manager = "apt"
+    elif os.path.exists("/usr/bin/dnf"):
+        package_manager = "dnf"
+
+    for opt, _ in opts:
         if opt in ("-h", "--help"):
-            print("""
+            print(
+                """
             -h/--help - This page
             -d/--database - Run database
             -i/--install - Setup app for running
             -f/--fix - Rebuild the database"""
             )
         elif opt in ("-d", "--database"):
-            sp.run(RUN_DB_CMD)
+            sp.run(run_db_cmd(package_manager))
         elif opt in ("-f", "--fix"):
             warnings.warn("It may be necessary to run script in superuser mode (Linux)")
             warnings.warn(
@@ -39,11 +50,9 @@ def main(argv):
             shutil.rmtree("./var/")
 
             # Build everything from the ground up
-            sp.run(RUN_DB_CMD)
+            sp.run(run_db_cmd(package_manager))
         elif opt in ("-i", "--install"):
-            if os.path.exists("/usr/bin/apt"):
-                package_manger = "apt"
-
+            if package_manager == "apt":
                 # Add Liberica repositories
                 os.system(
                     """wget -q -O - https://download.bell-sw.com/pki/GPG-KEY-bellsoft | sudo apt-key add -"""
@@ -52,40 +61,40 @@ def main(argv):
                     """echo "deb https://apt.bell-sw.com/ stable main" | sudo tee /etc/apt/sources.list.d/bellsoft.list"""
                 )
 
-            elif os.path.exists("/usr/bing/dnf"):
-                package_manger = "dnf"
-
+            elif package_manager == "dnf":
                 # Add Liberica repositories
-                os.system(
-                    """echo | sudo tee /etc/yum.repos.d/bellsoft.repo > /dev/null << EOF
-                    [BELLSOFT]
-                    name=BELLSOFT Repository
-                    baseurl=https://yum.bell-sw.com
-                    enabled=1
-                    gpgcheck=1
-                    gpgkey=https://download.bell-sw.com/pki/GPG-KEY-bellsoft
-                    priority=1
-                    EOF"""
-                )
+                cmd = "echo | sudo tee /etc/yum.repos.d/bellsoft.repo > /dev/null << EOF\n[BELLSOFT]\nname=BELLSOFT Repository\nbaseurl=https://yum.bell-sw.com\nenabled=1\ngpgcheck=1\ngpgkey=https://download.bell-sw.com/pki/GPG-KEY-bellsoft\npriority=1\nEOF"
+                os.system(cmd)
 
-            # Install docker & docker-compose & liberica17
-            sp.run(["sudo", package_manger, "update"])
+            # Install dependencies
+            sp.run(["sudo", package_manager, "update"])
             sp.run(
                 [
                     "sudo",
-                    package_manger,
+                    package_manager,
                     "install",
                     "docker",
                     "docker-compose",
+                    "postgresql",
                     "bellsoft-java17-full",
                 ]
             )
 
+            # Start docker temporarily
+            sp.run(["sudo", "systemctl", "start", "docker.service"])
+
+            # Run init.sql
+            sp.run(["psql", "-U", "postgres", "-a" "-f" "./Docker/init.sql"])
+
             # Database in the background
-            sp.Popen(RUN_DB_CMD)
+            sp.Popen(run_db_cmd(package_manager))
 
             # Run the app
-            liberica = "/usr/lib/jvm/bellsoft-java17-full-amd64/bin/java"
+            liberica = (
+                "/usr/lib/jvm/bellsoft-java17-full.x86-64"
+                if package_manager == "dnf"
+                else "/usr/lib/jvm/bellsoft-java17-full-amd64/bin/java"
+            )
             sp.run([liberica, "-jar", "pap.jar"])
 
 
