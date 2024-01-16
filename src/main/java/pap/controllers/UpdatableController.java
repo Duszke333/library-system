757b8c18360c5a0a78baf7df26837b1a0f404d63
pjablons <01178530@pw.edu.pl>
2024-01-16 20:13:42 +0100

    Comments to update()

--- src/main/java/pap/controllers/UpdatableController.java
+++ src/main/java/pap/controllers/UpdatableController.java
@@ -5,5 +5,14 @@ package pap.controllers;
  * This method allows for pages to update their values every time they are displayed.
  */
 public interface UpdatableController {
+    /**
+     * This method updates the controller. Implementation is completely dependent
+     * on the implementing controller, so there is no provided default. <br><br>
+     * 
+     * Good design would be to implement action that <b>makes sense </b>
+     * to be called frequently and whenever there is a <b>need</b> to
+     * update visible or non-visible content. Something akin to setting the labels
+     * to <i>empty</i> upon visiting the page is recommended.
+     */
     void update();
 }
