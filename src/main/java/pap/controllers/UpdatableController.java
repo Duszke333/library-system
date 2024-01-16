d68bf59e60ffdc04b5812cee541f83a2d83c24fa
pjablons <01178530@pw.edu.pl>
2024-01-16 20:25:36 +0100

    Fix

--- src/main/java/pap/controllers/UpdatableController.java
+++ src/main/java/pap/controllers/UpdatableController.java
@@ -2,12 +2,11 @@ package pap.controllers;
 
 /**
  * Interface that is implemented by every controller class that needs to be updated.
- * This method allows for pages to update their values every time they are displayed.
  */
 public interface UpdatableController {
     /**
-     * This method updates the controller. Implementation is completely dependent
-     * on the implementing controller, so there is no provided default. <br><br>
+     * This method allows for pages to update their values. Implementation is completely 
+     * dependent on the implementing controller, so there is no provided default. <br><br>
      * 
      * Good design would be to implement action that <b>makes sense </b>
      * to be called frequently and whenever there is a <b>need</b> to
