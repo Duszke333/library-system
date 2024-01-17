dd86a2a9c2bee123da17848f3a449cd9961a2472
pwysocki <01178616@pw.edu.pl>
2024-01-17 19:32:53 +0100

    Comment buff

--- src/main/java/pap/helpers/ConstraintViolationException.java
+++ src/main/java/pap/helpers/ConstraintViolationException.java
@@ -2,7 +2,7 @@ package pap.helpers;
 
 public class ConstraintViolationException extends Exception{
     /**
-     * An exception class that is thrown when the data given by the user is invalid.
+     * An exception class that is thrown when the data to be uploaded to / updated in the database is invalid.
      */
     public ConstraintViolationException(String message) {
         super(message);
